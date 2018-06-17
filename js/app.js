f8f19101b62e8bb44d0561f2859e98bacb522f35
Jim Kianese <jkianese@gmail.com>
2018-06-17 19:58:56 -0400

    First commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@
  * Create a list that holds all of your cards
  */
 
+const deck = document.querySelector('.deck');
+let = toggledCards = [];
+let moves = 0; 
+let clockOff = true;
+let time = 0; 
+let clockId;
+
+function shuffleDeck(){
+    const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+    const shuffledCards = shuffle(cardsToShuffle);
+    for (card of shuffledCards) {
+        deck.appendChild(card); 
+    }
+}
+shuffleDeck();
 
 /*
  * Display the cards on the page
@@ -22,6 +37,7 @@ function shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
 
+    
     return array;
 }
 
@@ -36,3 +52,157 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+deck.addEventListener('click', event => {
+    const clickTarget = event.target;
+    if (isClickValid(clickTarget)) {
+        if (clockOff) {
+            startClock();
+            clockOff = false; 
+        }
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if (toggledCards.length === 2) {
+            checkForMatch(clickTarget);
+            addMove();
+            checkScore(); 
+        }
+    }
+});
+
+function isClickValid(clickTarget) {
+    return (
+        clickTarget.classList.contains('card') &&
+        !clickTarget.classList.contains('match') &&
+        toggledCards.length < 2 &&
+        !toggledCards.includes(clickTarget)
+    );
+}
+
+function toggleCard(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+}
+
+function addToggleCard(clickTarget) {
+    toggledCards.push(clickTarget);
+}
+
+function checkForMatch() {
+    if (
+        toggledCards[0].firstElementChild.className === 
+        toggledCards[1].firstElementChild.className
+    ) {
+        toggledCards[0].classList.toggle('match');
+        toggledCards[1].classList.toggle('match');
+        toggledCards = [];
+    } else {
+        setTimeout(() => {
+            toggleCard(toggledCards[0]);
+            toggleCard(toggledCards[1]);
+            toggledCards = [];
+        }, 1000); 
+    }
+}
+
+function addMove() {
+    moves++;
+    const movesText = document.querySelector('.moves');
+    movesText.innerHTML = moves;
+}
+
+function checkScore() {
+    if (moves === 16 || moves === 24) {
+        hideStar();
+    }
+}
+
+function hideStar() {
+    starList = document.querySelectorAll('.stars li');
+    for (star of starList) {
+        if (star.style.display !== 'none') {
+            star.style.display = 'none';
+            break;
+        }
+    }
+}
+hideStar();
+hideStar();
+
+function startClock() {
+    clockId = setInterval(() => {
+        time++;
+        displayTime();
+    }, 1000);
+}
+
+function displayTime() {
+    const clock = document.querySelector('.clock');
+    const minutes = Math.floor(time / 60); 
+    const seconds = time % 60;
+    
+    if (seconds < 10) {
+        clock.innerHTML = `${minutes}:0${seconds}`;
+    } else {
+        clock.innerHTML = `${minutes}:${seconds}`;
+    }
+}
+
+function stopClock() {
+    clearInterval(clockId);
+}
+
+function toggleModal() {
+    const modal = document.querySelector('.modal_background');
+    modal.classList.toggle('hide');
+}
+toggleModal(); // Open modal
+toggleModal(); // Close modal
+
+function writeModalStats() {
+    const timeStat = document.querySelector('.modal_time');
+    const clockTime = document.querySelector('.clock').innerHTML;
+    const movesStat = document.querySelector('.modal_moves');
+    const starsStat = document.querySelector('.modal_stars');
+    const stars = getStars();
+
+    // template literals
+    timeStat.innerHTML = `Time = ${clockTime}`;
+    movesStat.innerHTML = `Moves = ${moves}`;
+    starsStat.innerHTML = `Stars = ${stars}`;
+}
+
+function getStars() {
+    stars = document.querySelectorAll('.stars li');
+    starCount = 0; 
+    for (star of stars) {
+        if (star.style.display !== 'none') {
+            starCount++;
+        }
+    }
+    return starCount;
+}
+
+toggleModal() //open modal
+toggleModal() //close modal
+
+// Modal tests
+time = 121;
+displayTime(); //2:01
+moves = 16;
+checkScore(); // 2 stars
+
+writeModalStats(); // write stats to modal
+toggleModal(); //open modal
+
+document.querySelector('.modal_cancel').addEventListener('click', () => {
+    toggleModal();
+});
+
+document.querySelector('.modal_replay').addEventListener('click', () => {
+    console.log('replay');
+});
+
+document.querySelector('.modal_replay').addEventListener('click', () => {
+    // TODO: Call reset game here
+});
